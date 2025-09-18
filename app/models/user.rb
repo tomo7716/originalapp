68b25974a4bfd16371142f0846a438ea0fcb90f9
tomo7716 <t19870106.ak@gmail.com>
2025-09-18 18:35:17 +0900

    管理者アカウント登録修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   validate :must_have_at_least_one_student, unless: :admin?
   validates :name, presence: true
 
+  after_create :set_admin_if_matches_env   # ← 追加
+
   def total_points
     students.sum(:points)
   end
@@ -19,6 +21,14 @@ class User < ApplicationRecord
   def must_have_at_least_one_student
     if students.blank? || students.all? { |s| s.marked_for_destruction? || s.name.blank? }
       errors.add(:base, "生徒は1名以上登録してください")
+    end
+  end
+
+  # --- 管理者昇格処理 ---
+  def set_admin_if_matches_env
+    if ENV["ADMIN_EMAIL"].present? && self.email == ENV["ADMIN_EMAIL"]
+      update(admin: true)
+      Rails.logger.info "✅ 管理者アカウントに昇格: #{self.email}"
     end
   end
 end
