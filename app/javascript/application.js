f0e8fded23cebce0cd8d3e306e0329aeffabc29b
tomo7716 <t19870106.ak@gmail.com>
2025-09-18 20:55:07 +0900

    viewの修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,9 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "./controllers/registrations"
 
+import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "@hotwired/stimulus-loading"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
