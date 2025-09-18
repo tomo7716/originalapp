390b21294c56f921a708b3b181d48f9d7123674f
tomo7716 <t19870106.ak@gmail.com>
2025-09-18 21:08:00 +0900

    viewの修正2

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,5 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Configure your import map in config/importmap.rb. 
+// Read more: https://github.com/rails/importmap-rails
+
 import "@hotwired/turbo-rails"
 import "controllers"
-import "./controllers/registrations"
-
-import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "@hotwired/stimulus-loading"
-
-const application = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
