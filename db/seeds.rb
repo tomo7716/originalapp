c5cb18034641eb1ba359a8dffc4f1b1ef2a6ea2a
tomo7716 <t19870106.ak@gmail.com>
2025-09-18 16:47:10 +0900

    管理者アカウント登録

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,21 +12,17 @@ Item.create!(name: "商品2", points: 2200, image_url: "sample2.png")
 Item.create!(name: "商品3", points: 3100, image_url: "sample3.png")
 
 # --- 管理者アカウント作成 ---
-admin_email = ENV["ADMIN_EMAIL"]
-admin_password = ENV["ADMIN_PASSWORD"]
-
-if admin_email.present? && admin_password.present?
-  unless User.exists?(email: admin_email)
+if ENV["ADMIN_EMAIL"].present? && ENV["ADMIN_PASSWORD"].present?
+  unless User.exists?(email: ENV["ADMIN_EMAIL"])
     User.create!(
       name: "管理者",
-      email: admin_email,
-      password: admin_password,
+      email: ENV["ADMIN_EMAIL"],
+      password: ENV["ADMIN_PASSWORD"],
+      password_confirmation: ENV["ADMIN_PASSWORD"],
       admin: true
     )
-    puts "管理者アカウントを作成しました: #{admin_email}"
+    puts "管理者アカウント作成完了"
   else
-    puts "管理者アカウントはすでに存在します: #{admin_email}"
+    puts "管理者アカウントは既に存在します"
   end
-else
-  puts "ADMIN_EMAIL または ADMIN_PASSWORD が設定されていません。管理者アカウントは作成されません。"
 end
